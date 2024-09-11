3b2708401abf6b7ea183fa8084023491340f7710
JeaMinLim <gam1532@gmail.com>
2024-09-11 23:36:38 +0900

    2024-09-11 add upload locaton parameter

--- src/main/java/com/jmlim0727/photouploader/PhotouploaderApplication.java
+++ src/main/java/com/jmlim0727/photouploader/PhotouploaderApplication.java
@@ -3,11 +3,28 @@ package com.jmlim0727.photouploader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+
+
 @SpringBootApplication
-public class PhotouploaderApplication {
+public class PhotouploaderApplication implements CommandLineRunner {
+
+	@Value("${location}")
+    private String LOCATION;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotouploaderApplication.class, args);
 		
 	}
+
+	@Override
+    public void run(String... args) throws Exception {
+        if (LOCATION == null || LOCATION.trim().isEmpty()) {
+            System.err.println("Error: --LOCATION is required.");
+            System.exit(1); // 애플리케이션 종료
+        }
+
+        System.out.println("--LOCATION : " + LOCATION);
+    }
 }
