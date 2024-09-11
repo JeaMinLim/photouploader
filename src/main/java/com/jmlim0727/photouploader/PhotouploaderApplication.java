96ce1c59cfb417a1717295c399b2a0be9ff2c082
JeaMinLim <gam1532@gmail.com>
2024-09-12 00:18:03 +0900

    2024-09-12 add a logic for checking entered location is accessible directory

--- src/main/java/com/jmlim0727/photouploader/PhotouploaderApplication.java
+++ src/main/java/com/jmlim0727/photouploader/PhotouploaderApplication.java
@@ -1,17 +1,20 @@
 package com.jmlim0727.photouploader;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhotouploaderApplication implements CommandLineRunner {
 
 	@Value("${location}")
     private String LOCATION;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotouploaderApplication.class, args);
@@ -22,9 +25,16 @@ public class PhotouploaderApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         if (LOCATION == null || LOCATION.trim().isEmpty()) {
             System.err.println("Error: --LOCATION is required.");
-            System.exit(1); // 애플리케이션 종료
+            System.exit(1); 
         }
-
         System.out.println("--LOCATION : " + LOCATION);
+        
+        Path PATH = Paths.get(LOCATION);
+        if(Files.exists(PATH) && Files.isDirectory(PATH) && Files.isReadable(PATH)) {
+            System.out.println("Directory is accessible.");
+        } else {
+            System.err.println("Error: Directory is not accessible.");
+            System.exit(1);
+        }
     }
 }
